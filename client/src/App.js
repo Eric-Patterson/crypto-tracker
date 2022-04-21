349e62af5c7a4fd511f19a3a646a3bc3b12b4acd
Eric Patterson <ericpattersonwork@gmail.com>
2022-04-20 23:35:09 -0600

    adding boilerplate code for project

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [crypto, setCrypto] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api").then((res) => {
+      console.log(res.data);
+      setCrypto(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    axios.get("/api").then((res) => {
+      console.log(res.data);
+      setCrypto(res.data);
+    });
+  }, []);
+
+  // test
+  // here is some more code
+  return (
+    <div>
+      {/* display */}
+      {/* loop over data.data[i].id */}
+      {crypto &&
+        crypto.map((coin) => (
+          <div>
+            <h1>{coin.id}</h1>
+            <img src={coin.image.thumb}></img>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const axios = require("axios");
+const CoinGecko = require("coingecko-api");
+const CoinGeckoClient = new CoinGecko();
+// crypto api
+// https://www.coingecko.com/en/api/documentation?
+// https://github.com/miscavage/CoinGecko-API
+
+const app = express();
+const port = 5000;
+
+app.get("/api", async (req, res) => {
+  // https://api.coingecko.com/api/v3/coins
+  // get api and res.send response
+  let response = await axios.get("https://api.coingecko.com/api/v3/coins");
+  res.send(response.data);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
